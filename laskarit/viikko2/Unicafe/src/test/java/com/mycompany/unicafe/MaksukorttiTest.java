d40e6e21078953ca0527305a0b77687f13494366
k0tix <otto.pirttimaa@gmail.com>
2019-03-23 01:48:34 +0200

    lisää unicafe maksukortti testejä

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,42 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void konstruktoriAsettaaSaldonOikein() {
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void rahanOttoVahentaaSaldoaOikein() {
+        kortti.otaRahaa(2);
+        assertEquals(8, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosEiTarpeeksiRahaa() {
+        kortti.otaRahaa(100);
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void palauttaaTrueJosOttoOnnistui() {
+        assertTrue(kortti.otaRahaa(2));
+    }
+    
+    @Test
+    public void palauttaaFalseJosOttoEpaonnistui() {
+        assertFalse(kortti.otaRahaa(100));
+    }
+    
+    @Test
+    public void toStringPalauttaaOikeatArvot() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(100);
+        assertEquals(110, kortti.saldo());
+    }
 }
