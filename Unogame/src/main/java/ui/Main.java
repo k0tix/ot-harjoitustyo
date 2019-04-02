2ab1c80da7c020abf794ce2c2398fdc0040665e2
k0tix <otto.pirttimaa@gmail.com>
2019-04-02 22:57:40 +0300

    lisää pelaajamäärän kysyminen

--- Unogame/src/main/java/ui/Main.java
+++ Unogame/src/main/java/ui/Main.java
@@ -1,6 +1,7 @@
 package ui;
 
 import domain.Deck;
+import java.util.Scanner;
 
 /**
  *
@@ -12,6 +13,32 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        Scanner read = new Scanner(System.in);
+        System.out.println("UNOGAME");
+        System.out.println("How many player will be playing (2-10)?");
+        
+        int playerAmount = 0;
+        
+        while(true) {
+            String players = read.nextLine();
+            
+            try {
+                int amount = Integer.parseInt(players);
+                
+                if(amount < 2 || amount > 10) {
+                    System.out.println("There can only be 2-10 players!");
+                } else {
+                    playerAmount = amount;
+                    break;
+                }
+                
+            } catch (Exception e) {
+                System.out.println("Only valid integers allowed as input!");
+            }
+        }
+        
+        System.out.println(String.format("##########\n%d players\n##########", playerAmount));
         
         Deck deck = new Deck();
         System.out.println(deck.isEmpty());
