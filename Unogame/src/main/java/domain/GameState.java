f4430302d7cce805d8e30890fb7f2a0509184179
k0tix <otto.pirttimaa@gmail.com>
2019-04-16 22:54:08 +0300

    korjaa checkstyle virheitä ja jätä käyttöliittymä huomiotta checkstylessä

---
+++ Unogame/src/main/java/domain/GameState.java
@@ -0,0 +1,19 @@
+package domain;
+
+/**
+ *
+ * @author k0tix
+ */
+public class GameState {
+    private String direction;
+    
+    
+    public GameState(int playerAmount) {
+    }
+    
+    public String getDirection() {
+        return this.direction;
+    }
+    
+    
+}
--- Unogame/src/main/java/domain/Uno.java
+++ Unogame/src/main/java/domain/Uno.java
@@ -1,9 +1,50 @@
 package domain;
 
-/**
- *
- * @author k0tix
- */
+import java.util.ArrayList;
+
 public class Uno {
+    private ScoreBoard score;
+    private ArrayList<Player> players;
+    private int playerAmount;
+    
+    public Uno(ScoreBoard scoreBoard, ArrayList<Player> players) {
+        this.score = scoreBoard;
+        this.players = players;
+    }
+    
+    public Uno() {
+        
+    }
+    
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+    
+    public ArrayList<Player> getPlayers() {
+        return this.players;
+    }
+    
+    public void setPlayerAmount(int amount) {
+        this.playerAmount = amount;
+    }
+    
+    public void playRound(int startingPoint) {
+        Deck deck = new Deck();
+        deck.initializeCards();
+        
+        String direction = "clockwise";
+        
+        initializePlayersCards(deck);
+        
+        Card lastPlayedCard = deck.pick();
+    }
+    
+    public void initializePlayersCards(Deck deck) {
+        for (Player p : this.players) {
+            for (int i = 0; i < 7; i++) {
+                p.giveCard(deck.pick());
+            }
+        }
+    }
     
 }
