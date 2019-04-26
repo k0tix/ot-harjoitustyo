bae0551852714ab198c3842e757b874f0fdef329
k0tix <otto.pirttimaa@gmail.com>
2019-04-26 23:20:49 +0300

    lisää testejä Uno luokalle

---
+++ Unogame/src/test/java/domain/UnoTest.java
@@ -0,0 +1,62 @@
+package domain;
+
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author k0tix
+ */
+public class UnoTest {
+    
+    Uno uno;
+    ArrayList<Player> players;
+    
+    @Before
+    public void setUp() {
+        players = new ArrayList<>();
+        players.add(new Player(("Pekka")));
+        players.add(new Player(("Matti")));
+        players.add(new Player(("Kerttu")));
+        
+        uno = new Uno(new ScoreBoard(players), players);
+    }
+
+    @Test
+    public void directionIsCorrectAfterInitialization() {
+        assertEquals("clockwise", uno.getDirection());
+    }
+    
+    @Test
+    public void firstPlayerIsCorrectAfterInitialization() {
+        assertEquals(players.get(0), uno.getCurrentPlayer());
+    }
+    
+    @Test
+    public void directionIsChangedCorrectly() {
+        uno.changeDirection();
+        assertEquals("anticlockwise", uno.getDirection());
+    }
+    
+    @Test
+    public void nextPlayerIsCalculatedCorrectly() {
+        assertEquals(players.get(0), uno.getCurrentPlayer());
+        uno.nextPlayer();
+        assertEquals(players.get(1), uno.getCurrentPlayer());
+    }
+    
+    @Test
+    public void lastPlayerIsCalculatedCorrectly() {
+        assertEquals(players.get(0), uno.getCurrentPlayer());
+        uno.lastPlayer();
+        assertEquals(players.get(2), uno.getCurrentPlayer());
+    }
+    
+    @Test
+    public void correctAmountOfCardsAreDealt() {
+        uno.startRound(0);
+        assertEquals(7, uno.getPlayers().get(0).getCards().size());
+    }
+}
