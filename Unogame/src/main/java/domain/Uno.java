5fd091b935f79c1fede4cb6b68eb2505fed81506
k0tix <otto.pirttimaa@gmail.com>
2019-04-26 23:38:56 +0300

    muotoilun korjausta

--- Unogame/src/main/java/domain/Uno.java
+++ Unogame/src/main/java/domain/Uno.java
@@ -7,51 +7,54 @@ import domain.Card.*;
  * Luokka tarjoaa toiminnallisuutta Uno-pelin pelilogiikkaan ja vuorojen kulkuun
  */
 public class Uno {
+
     private ScoreBoard score;
     private ArrayList<Player> players;
-    
+
     private boolean direction; //true = clockwise, false = anticlockwise
     private int currentPlayer;
     private Card lastPlayedCard;
     private Deck deck;
-    
+
     public Uno(ScoreBoard scoreBoard, ArrayList<Player> players) {
         this.score = scoreBoard;
         this.players = players;
         this.direction = true;
         this.currentPlayer = 0;
     }
-    
+
     public Uno() {
         this(null, new ArrayList<>());
     }
-    
+
     public void setPlayers(ArrayList<Player> players) {
         this.players = players;
     }
-    
+
     public ArrayList<Player> getPlayers() {
         return this.players;
     }
-    
+
     /**
-     * Metodi aloittaa yksittäisen pelin ja luo sitä varten uuden pakan
-     * ja jakaa aloituskortit pelaajille
+     * Metodi aloittaa yksittäisen pelin ja luo sitä varten uuden pakan ja jakaa
+     * aloituskortit pelaajille
+     *
      * @param startingPoint pelaaja, josta aloitetaan
      */
     public void startRound(int startingPoint) {
         this.deck = new Deck();
         this.deck.initializeCards();
-        
+
         initializePlayersCards(deck);
-        
+
         this.lastPlayedCard = deck.pick();
     }
-    
+
     /**
-     * Metodi pelaa yhden pelaajan valitseman kortin, asettaa sen päällimäiseksi ja 
-     * muuttaa pelin tilaa ja muiden pelaajien korttien määrä ja pelivuoroa pelatun kortin
-     * mukaan
+     * Metodi pelaa yhden pelaajan valitseman kortin, asettaa sen päällimäiseksi
+     * ja muuttaa pelin tilaa ja muiden pelaajien korttien määrä ja pelivuoroa
+     * pelatun kortin mukaan
+     *
      * @param card pelattu kortti
      * @param index kortin indeksi
      * @return onnistuiko kortin pelaaminen
@@ -60,10 +63,10 @@ public class Uno {
         if (!isCardPlayable(card)) {
             return false;
         }
-        
+
         this.lastPlayedCard = this.players.get(currentPlayer).playCard(index);
         nextPlayer();
-        
+
         Type t = this.lastPlayedCard.getType();
         if (t.equals(Type.DRAW_TWO)) {
             getCurrentPlayer().giveCard(deck.pick());
@@ -78,12 +81,13 @@ public class Uno {
         } else if (t.equals(Type.REVERSE)) {
             lastPlayer();
         }
-        
+
         return true;
     }
-    
+
     /**
      * Metodi kertoo voiko kyseisen kortin pelata viimeksi pelatun kortin päälle
+     *
      * @param card pelattava kortti
      * @return voiko kortin pelata
      */
@@ -99,17 +103,18 @@ public class Uno {
         } else if (card.getType().equals(lastPlayedCard.getType()) && !card.getType().equals(Type.NUMBER)) {
             return true;
         }
-        
+
         return false;
     }
-    
+
     public Player getCurrentPlayer() {
         return this.players.get(currentPlayer);
     }
-    
+
     /**
      * Metodi jakaa pelaajille 7 aloituskorttia
-     * @param deck pelissä käytettävä pakka 
+     *
+     * @param deck pelissä käytettävä pakka
      */
     public void initializePlayersCards(Deck deck) {
         for (Player p : this.players) {
@@ -119,38 +124,38 @@ public class Uno {
             System.out.println(p.getCards().size());
         }
     }
-    
+
     public Card getLastPlayedCard() {
         return this.lastPlayedCard;
     }
-    
+
     public String getDirection() {
         return this.direction ? "clockwise" : "anticlockwise";
     }
-    
+
     /**
      * Metodi vaihtaa pelin suuntaa
      */
     public void changeDirection() {
         this.direction = !direction;
     }
-    
+
     /**
      * Metodi siirtää vuoron seuraavalle pelaajalle
      */
     public void nextPlayer() {
-       if(this.currentPlayer + 1 < players.size()) {
-           this.currentPlayer += 1;
-       } else {
-           this.currentPlayer = 0;
-       }
+        if (this.currentPlayer + 1 < players.size()) {
+            this.currentPlayer += 1;
+        } else {
+            this.currentPlayer = 0;
+        }
     }
-    
+
     /**
      * Metodi siirtää vuoron edelliselle pelaajalle
      */
     public void lastPlayer() {
-        if(this.currentPlayer - 1 < 0) {
+        if (this.currentPlayer - 1 < 0) {
             this.currentPlayer = players.size() - 1;
         } else {
             this.currentPlayer -= 1;
