828ec97ff7931a46e1ae328914ae6d8a21d9dd82
k0tix <otto.pirttimaa@gmail.com>
2019-05-05 15:30:43 +0300

    lisää metodit eri korttityyppien lisäämiselle

--- Unogame/src/main/java/domain/Deck.java
+++ Unogame/src/main/java/domain/Deck.java
@@ -10,77 +10,93 @@ import java.util.Collections;
  * Luokka toteuttaa korttipakan
  */
 public class Deck {
+
     private ArrayList<Card> deck;
-    
+
     public Deck() {
         this.deck = new ArrayList<>();
     }
-    
+
     /**
      * Metodi sekoittaa korttipakan
      */
     public void shuffle() {
         Collections.shuffle(deck);
     }
-    
+
     /**
      * Metodi palauttaa pakan päällimäisen kortin ja poistaa sen korttipakasta
+     *
      * @return päällimainen kortti
      */
     public Card pick() {
         return this.deck.remove(0);
     }
-    
+
     /**
      * Metodi kerto onko korttipakassa vielä kortteja
+     *
      * @return onko tyhjä
      */
     public boolean isEmpty() {
         return this.deck.isEmpty();
     }
-    
+
     /**
-     * Metodi alustaa korttipakan ja lisää sinne Uno-pakassa
-     * olevat kortit ja lopuksi sekoittaa pakan
+     * Metodi alustaa korttipakan ja lisää sinne Uno-pakassa olevat kortit ja
+     * lopuksi sekoittaa pakan
      */
     public void initializeCards() {
         this.deck.clear();
-                
-        //add number cards, 1x 0 for each color and 2x other numbers
+
+        addNumberCards();
+        addActionCards();
+        addWildCards();
+
+        shuffle();
+    }
+
+    /**
+     * Metodi lisää pakkaan numerokortit
+     */
+    private void addNumberCards() {
         Arrays.stream(Color.values()).forEach(color -> {
             if (color.equals(Color.WILD)) {
                 return;
             }
             this.deck.add(new Card(Type.NUMBER, color, Value.ZERO));
-            
+
             for (int num = 1; num < 10; num++) {
                 this.deck.add(new Card(Type.NUMBER, color, Value.values()[num]));
                 this.deck.add(new Card(Type.NUMBER, color, Value.values()[num]));
             }
         });
-        
-        //add action cards
+    }
+
+    /**
+     * Metodi lisää pakkaan toimintakortit
+     */
+    private void addActionCards() {
         Arrays.stream(Color.values()).forEach(color -> {
             if (color.equals(Color.WILD)) {
                 return;
             }
             this.deck.add(new Card(Type.SKIP, color, Value.SPECIAL));
             this.deck.add(new Card(Type.SKIP, color, Value.SPECIAL));
-            
             this.deck.add(new Card(Type.REVERSE, color, Value.SPECIAL));
             this.deck.add(new Card(Type.REVERSE, color, Value.SPECIAL));
-            
             this.deck.add(new Card(Type.DRAW_TWO, color, Value.SPECIAL));
             this.deck.add(new Card(Type.DRAW_TWO, color, Value.SPECIAL));
         });
-        
-        //add wild cards
+    }
+
+    /**
+     * Metodi lisää pakkaan villikortit
+     */
+    private void addWildCards() {
         for (int i = 0; i < 4; i++) {
             this.deck.add(new Card(Type.CHANGE_COLOR, Color.WILD, Value.SPECIAL));
-            
             this.deck.add(new Card(Type.DRAW_FOUR, Color.WILD, Value.SPECIAL));
         }
-        
-        shuffle();
     }
 }
