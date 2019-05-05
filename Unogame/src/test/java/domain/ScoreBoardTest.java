aa7f2e80a5845c7cb49ff23583a1542d9c2c01ba
k0tix <otto.pirttimaa@gmail.com>
2019-05-05 23:28:40 +0300

    lisää testi pistetaulun tulosteelle

--- Unogame/src/test/java/domain/ScoreBoardTest.java
+++ Unogame/src/test/java/domain/ScoreBoardTest.java
@@ -13,17 +13,17 @@ import static org.junit.Assert.*;
  * @author k0tix
  */
 public class ScoreBoardTest {
-    
+
     ScoreBoard score;
     ArrayList<Player> players;
-    
+
     @Before
     public void setUp() {
         players = new ArrayList<>();
         players.add(new Player(("Pekka")));
         players.add(new Player(("Matti")));
         players.add(new Player(("Kerttu")));
-        
+
         score = new ScoreBoard(players);
     }
 
@@ -31,19 +31,33 @@ public class ScoreBoardTest {
     public void scoreBoardReturnZeroAsStartScore() {
         assertEquals(0, score.getScore(players.get(0)));
     }
-    
+
     @Test
     public void scoreBoardUpdatesScoreCorrectly() {
         HashMap<Player, Integer> newScores = new HashMap<>();
         newScores.put(players.get(0), 5);
         newScores.put(players.get(1), 2);
         newScores.put(players.get(2), 1);
-        
+
         score.updateScores(newScores);
-        
+
         assertEquals(5, score.getScore(players.get(0)));
         assertEquals(2, score.getScore(players.get(1)));
         assertEquals(1, score.getScore(players.get(2)));
     }
-    
+
+    @Test
+    public void toStringReturnsCorrectValues() {
+        HashMap<Player, Integer> newScores = new HashMap<>();
+        newScores.put(players.get(0), 20);
+        newScores.put(players.get(1), 50);
+        newScores.put(players.get(2), 4);
+
+        score.updateScores(newScores);
+
+        assertEquals("Scores: \n"
+                + " Player Kerttu: 4\n"
+                + " Player Pekka: 20\n"
+                + " Player Matti: 50", score.toString());
+    }
 }
